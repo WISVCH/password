d0e7b8190ffa046ef1e9c51764e103722f6d564e
Mark Janssen <mark@praseodym.net>
2016-10-30 14:33:03 +0100

    Update test for Spring Boot 1.4

--- src/test/java/ch/wisv/password/PasswordApplicationTests.java
+++ src/test/java/ch/wisv/password/PasswordApplicationTests.java
@@ -2,13 +2,11 @@ package ch.wisv.password;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PasswordApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PasswordApplication.class)
 public class PasswordApplicationTests {
 
     @Test
